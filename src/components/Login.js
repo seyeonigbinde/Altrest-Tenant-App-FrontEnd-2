cffbcd5fcfeba8c6b37d6354b65ba8507b006847
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-06-23 04:43:24 +0100

    Login Bugs Fixed

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,7 @@ const credentials = {
   password: ""
 }
 const Login = () => {
-  
+
   const { push } = useHistory();
 
   const [userLogin, setUserLogin] = useState(credentials)
@@ -88,81 +88,3 @@ const Login = () => {
 };
 
 export default Login;
-
-
-
-// import React, { useState } from 'react'
-// import { connect } from 'react-redux';
-// import { useHistory } from 'react-router-dom';
-// import { userLoginSuccess, userLoginFailure, userLoginStart } from '../store/actions';
-// import { axiosWithAuth } from '../utils/axiosWithAuth';
-
-
-
-// const LoginForm = (props) => {
-
-//     const { push } = useHistory();
-//     const [details, setDetails] = useState({username: "", password: ""});
-//     const { userLoginSuccess, userLoginFailure, isCallingAPI, error } = props;
-
-//     const submitHandler = evt => {
-//         evt.preventDefault();
-//         userLoginStart();
-//         axiosWithAuth()
-//           .post("/users/login", details)
-//           .then( res => {
-//             localStorage.setItem('token', JSON.stringify(res.data.access_token));
-//             userLoginSuccess(res.data.user);
-//             push(`/user/${res.data.user.user_id}/plants`);
-//           })
-//           .catch( err => {
-//             console.log(err);
-//             userLoginFailure(err);
-//           })
-
-//     }
-
-//     return (
-//       <div className="login">
-//         { (isCallingAPI) ? (<h2>Please wait, checking credentials</h2>): 
-//             (
-//             <form onSubmit={submitHandler}>
-//                 <StyledLogin>
-//                    <h2>Welcome Back!</h2>
-//                     {(error !== "") ? (<div className= "error">{error}</div>) : ""}
-//                     <div className= "form-group">
-//                         <label>Username: </label>
-//                         <input
-//                             type= "text"
-//                             name= "username"
-//                             placeholder= "Enter a username..."
-//                             onChange= {evt => 
-//                                 setDetails({...details, username: evt.target.value})} 
-//                                 value= {details.username}/>
-//                     </div>
-//                     <div className= "form-group">
-//                         <label>Password: </label>
-//                         <input  
-//                             type= "password"
-//                             name= "password"
-//                             placeholder= "Enter a password..."
-//                             onChange= {evt => 
-//                                 setDetails({...details, password: evt.target.value})} 
-//                                 value= {details.password}/>
-//                     </div>
-//                     <button id="submit" type="submit">Login</button>
-//                 </StyledLogin>
-//             </form>
-//             )}
-//       </div>
-//     )
-// }
-
-// const mapStateToProps = (state) => {
-//   return {
-//     isCallingAPI: state.user.isCallingAPI,
-//     error: state.user.error,
-//   }
-// }
-
-// export default connect(mapStateToProps, { userLoginSuccess, userLoginFailure, userLoginStart })(LoginForm);
