fa9ac8b557aaa3bc8fe9a6280ada42cdb80b3b48
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-08-21 13:20:47 +0100

    Footer Component Restructured

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,43 +4,44 @@ import { Link } from "react-router-dom"
 
 const Footer = () => {
 
-    return (
-        <footer className="footer">
-            <section className=" footer_body">
-                <div className="footer_links">
-                    <h4>Company</h4>
 
+
+    return (
+        <footer>
+            <section className="footer_container">
+                <div className="footer_items" >
+                    <h1><b> ALTREST </b></h1>
+                    <div className="footer_socials">
+                        <i class="fab fa-facebook-f"></i>
+                        <i class="fab fa-twitter"></i>
+                        <i class="fab fa-instagram"></i>
+                        <i class="fab fa-youtube"></i>
+                    </div>
+                </div>
+                <div className="footer_items" >
+                    <h4>Company</h4>
                     <Link to='/about'>About</Link>
                     <Link>Contact</Link>
                     <Link>Blog</Link>
                     <Link>Careers</Link>
                 </div>
-                <div className="footer_links">
-                    <h4>Services</h4>
-
-                    <Link>Landlords</Link>
-                    <Link>Tenants</Link>
-                    <Link>Community Associations</Link>
-                    <Link>Service Providers</Link>
-                </div>
-                <div className="footer_links">
+                <div className="footer_items" >
                     <h4>Resources</h4>
 
                     <Link>API Reference</Link>
                     <Link>Terms & Conditions</Link>
                     <Link>FAQs</Link>
                     <Link>Privacy & Security</Link>
+
+                </div>
+                <div className="footer_items" >
+                    <h3>About ALTREST</h3>
+                    <p>ALTREST is a digital facility management App that helps tenants and landlords manage communications, maintenance requests and payments. </p>
                 </div>
             </section>
-            <section className="footer_note" >
-                <div className="footer_copyright">
-                    <p>Copyright &copy; 2021 || Designed by Stevens Design Tech.</p>
-                </div>
-                <div className="footer_social">
-                    <i class="fab fa-facebook-f fa-2x primary" style={{ color: '#185adb' }}></i>
-                    <i class="fab fa-instagram fa-2x" style={{ color: '#fb3958' }}></i>
-                    <i class="fab fa-twitter fa-2x" style={{ color: 'skyblue' }}></i>
-                </div>
+            <hr />
+            <section className="footer_copyright">
+                <p>Copyright &copy; 2021 || Designed by Stevens Design Tech.</p>
             </section>
         </footer>
     )
